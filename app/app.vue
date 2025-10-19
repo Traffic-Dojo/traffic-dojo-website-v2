8658e98ed273778ff949499d71d03d8d16a2ca69
Vadim Ketov <kitketovsky@gmail.com>
2025-10-19 22:26:52 +0400

    nuxt main pages

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,5 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div class="bg-background text-white">
+    <NuxtPage />
   </div>
 </template>
---
+++ app/pages/index.vue
@@ -0,0 +1,58 @@
+<script setup lang="ts">
+import "swiper/css";
+import "swiper/css/pagination";
+
+import { Swiper, SwiperSlide } from "swiper/vue";
+
+import SlideIntroduction from "./../components/slides/SlideIntroduction.vue";
+import SlideEnthusiasts from "./../components/slides/SlideEnthusiasts.vue";
+import SlidePurpose from "./../components/slides/SlidePurpose.vue";
+import SlideProblems from "./../components/slides/SlideProblems.vue";
+import SlideProposalTime from "./../components/slides/SlideProposalTime.vue";
+import SlideWhatWeCanDo from "./../components/slides/SlideWhatWeCanDo.vue";
+import SlideBusiness from "./../components/slides/SlideBusiness.vue";
+import SlideFooter from "./../components/slides/SlideFooter.vue";
+
+import { MotionConfig } from "motion-v";
+import { useAppConfig } from "#app";
+
+const { initialSwiperConfig } = useAppConfig();
+</script>
+
+<template>
+  <MotionConfig :in-view-options="{ once: true }">
+    <Swiper v-bind="initialSwiperConfig">
+      <SwiperSlide>
+        <SlideIntroduction />
+      </SwiperSlide>
+
+      <SwiperSlide>
+        <SlideEnthusiasts />
+      </SwiperSlide>
+
+      <SwiperSlide>
+        <SlidePurpose />
+      </SwiperSlide>
+
+      <SwiperSlide>
+        <SlideBusiness />
+      </SwiperSlide>
+
+      <SwiperSlide>
+        <SlideProblems />
+      </SwiperSlide>
+
+      <SwiperSlide>
+        <SlideWhatWeCanDo />
+      </SwiperSlide>
+
+      <SwiperSlide>
+        <SlideProposalTime />
+      </SwiperSlide>
+
+      <SwiperSlide>
+        <SlideFooter />
+      </SwiperSlide>
+    </Swiper>
+  </MotionConfig>
+</template>
